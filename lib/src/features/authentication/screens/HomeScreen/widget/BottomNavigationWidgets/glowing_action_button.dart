e79ea9883bb4e4c08d3eb4604d9208bf0372f33d
tyagimadhur06 <tyagimadhur06@gmail.com>
2024-01-18 20:33:21 +0530

    Glowing action buttin updated

--- lib/src/features/authentication/screens/HomeScreen/widget/BottomNavigationWidgets/glowing_action_button.dart
+++ lib/src/features/authentication/screens/HomeScreen/widget/BottomNavigationWidgets/glowing_action_button.dart
@@ -8,40 +8,55 @@ class GlowingActionButton extends StatelessWidget {
     required this.icon,
     this.size = 54,
     required this.onPressed,
+    required this.label,
   }) : super(key: key);
 
   final Color color;
   final IconData icon;
   final double size;
   final VoidCallback onPressed;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: color.withOpacity(0.3),
-            spreadRadius: 8,
-            blurRadius: 24,
-          ),
-        ],
-      ),
-      child: ClipOval(
-        child: Material(
+    return SizedBox(
+      width: size,
+      height: size, // Adjust the height as needed to accommodate the text
+      child: Container(
+        decoration: BoxDecoration(
           color: color,
-          child: InkWell(
-            splashColor: LightColors.card,
-            onTap: onPressed,
-            child: SizedBox(
-              width: size,
-              height: size,
-              child: Icon(
-                icon,
-                size: 26,
-                color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: color.withOpacity(0.3),
+              spreadRadius: 8,
+              blurRadius: 24,
+            ),
+          ],
+        ),
+        child: ClipOval(
+          child: Material(
+            color: color,
+            child: InkWell(
+              splashColor: LightColors.card,
+              onTap: onPressed,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    icon,
+                    size: 26,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    label,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
